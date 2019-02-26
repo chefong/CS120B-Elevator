b696a6529a73ce2aaf9365d175e77f734e73d68a
ericong18 <ericong18@gmail.com>
2019-02-26 13:03:56 -0800

    created project

---
+++ elevator/main.c
@@ -0,0 +1,18 @@
+/*
+ * elevator.c
+ *
+ * Created: 2/26/2019 1:01:38 PM
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
